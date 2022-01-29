d2edc7edc58bf335a4292162307d8eec06d56d7b
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-01-29 16:34:25 +0000

    Added script for messaging bot

--- py/clear-your-head.py
+++ py/clear-your-head.py
@@ -2,3 +2,97 @@ import numpy as numpy
 import nltk
 import string
 import random
+import os
+import discord
+
+client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+
+starter_encouragements = [
+  “Being able to be your true self is one of the strongest components of good mental health.”,
+  “Being vulnerable is actually a strength and not a weakness — that’s why more and more mental health is such an important thing to talk about. It’s the same as being physically sick. And when you keep all those things inside, when you bottle them up, it makes you ill.”,
+  “Promise me you’ll always remember — you’re braver than you believe, and stronger than you seem, and smarter than you think.”,
+  “Not until we are lost do we begin to understand ourselves”,
+  “Tough times never last, but tough people do!”,
+  “There is hope, even when your brain tells you there isn’t.",
+]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragment(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith("$inspire"):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if db["responding"]:
+    options = starter_encouragements
+    if "encouragements" in db.keys():
+      options = options + db["encouragements"]
+
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
+
+  if msg.startswith("$new"):
+    encouraging_message = msg.split("$new ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+  if msg.startswith("$del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("$del",1)[1])
+      delete_encouragment(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith("$list"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+    
+  if msg.startswith("$responding"):
+    value = msg.split("$responding ",1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Responding is on.")
+    else:
+      db["responding"] = False
+      await message.channel.send("Responding is off.")
+
+client.run(os.getenv("TOKEN"))
+
+
