d5c94dc1a6c1a6fe4ccb9e319fdc2adaf6c29a75
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-01-29 13:58:31 +0000

    Add audio to delete button

--- js/clear-your-head.js
+++ js/clear-your-head.js
@@ -1,3 +1,13 @@
-function clearInput() {
+let deleteAudio = new Audio("audios/whoosh.flac");
+
+document.getElementById("clear-btn").addEventListener("click", deleteBtnClick);
+
+function deleteBtnClick() {
     document.getElementById("floatingTextarea2").value="";
+
+    deleteAudio.play();
+    deleteAudio.volume = 0.3;
+    deleteAudio.loop = false;
 }
+
+
