8332633fe57ec76002996130fa3eafab074eca57
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-01-31 10:39:41 +0000

    Remove unused elements in clear-your-head.js

--- static/js/clear-your-head.js
+++ static/js/clear-your-head.js
@@ -1,16 +1,15 @@
 let deleteTextSound = new Audio("static/audios/whoosh.flac");
 let completeTaskAudio = new Audio("static/audios/complete-task.mp3");
 let seaAudio = new Audio("static/audios/sea.flac");
-let cityParkAudio = new Audio("static/audios/city-park.wav");
 
 document.getElementById("clear-btn").addEventListener("click", clearBtnClick);
 document.getElementById("nature-audio-start").addEventListener("click", natureAudioStart);
 document.getElementById("nature-audio-pause").addEventListener("click", natureAudioPause);
-let fiveMinuteButton = document.getElementById("5-min-btn").addEventListener("click", initFiveMinute);
-let tenMinuteButton = document.getElementById("10-min-btn").addEventListener("click", initTenMinute);
-let fifteenMinuteButton = document.getElementById("15-min-btn").addEventListener("click", initFifteenMinute);
-let thirtyMinuteButton = document.getElementById("15-min-btn").addEventListener("click", initThirtyMinute);
-let sixtyMinuteButton = document.getElementById("15-min-btn").addEventListener("click", initSixtyMinute);
+document.getElementById("5-min-btn").addEventListener("click", initFiveMinute);
+document.getElementById("10-min-btn").addEventListener("click", initTenMinute);
+document.getElementById("15-min-btn").addEventListener("click", initFifteenMinute);
+document.getElementById("30-min-btn").addEventListener("click", initThirtyMinute);
+document.getElementById("1-hr-btn").addEventListener("click", initSixtyMinute);
 
 function clearBtnClick() {
     document.getElementById("floatingTextarea2").value = "";
@@ -27,22 +26,22 @@ function initFiveMinute() {
 }
 
 function initTenMinute() {
-    setInterval(barCount, 1200)
+    setInterval(barCount, 1200);
     taskAudio();
 }
 
 function initFifteenMinute() {
-    setInterval(barCount, 1800)
+    setInterval(barCount, 1800);
     taskAudio();
 }
 
 function initThirtyMinute() {
-    setInterval(barCount, 3600)
+    setInterval(barCount, 3600);
     taskAudio();
 }
 
 function initSixtyMinute() {
-    setInterval(barCount, 7200)
+    setInterval(barCount, 7200);
     taskAudio();
 }
 
